b5d24f3f43b8f117d5219fa19d5628e411b45b0d
IvanM3112 <98319071+IvvyDestiny@users.noreply.github.com>
2025-06-03 12:59:19 -0300

    fix: garante fallback para index.html em qualquer rota (Render)

--- SITE/server.js
+++ SITE/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, 'SITE')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'SITE', 'index.html'));
 });
 
